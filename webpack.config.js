6f1952354d94cc27dc7ec2c2e97be94ed4e6073c
Voro123 <2393317074@qq.com>
2020-07-30 20:00:44 +0800

    master

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './constructor.js',
+  entry: './static/js/_vue/constructor.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'static/js/_vue/dist')
   },
   mode: 'development',
   //处理es6 语法  exclude 排除 node_modules 下面的文件
